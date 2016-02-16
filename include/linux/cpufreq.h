d18fb9138ebd14939bace12b4e7ebf10a4692bcd
pulshen <pulshencode@outlook.com>
2016-02-16 17:46:36 +0200

    Fixed errors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -389,9 +389,6 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&amp;cpufreq_gov_smartass2)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTX)
-extern struct cpufreq_governor cpufreq_gov_smartx;
-#define CPUFREQ_DEFAULT_GOVERNOR (&amp;cpufreq_gov_smartx)
 
 
 
