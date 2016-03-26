d7e6ab671fde69a9a1bc3914133d7611cea1de0a
sudokamikaze <pulshencode@outlook.com>
2016-03-26 23:43:22 +0200

    scripts: Some fixes and updates

--- zip_creator.sh
+++ zip_creator.sh
@@ -15,23 +15,23 @@ case "$item" in
   1) echo "Creating default flash zip"
   cp scripts/updater-script-default META-INF/com/google/android/
   mv META-INF/com/google/android/updater-script-default META-INF/com/google/android/updater-script
-  zip XKernel-Flasher.zip -r META-INF presets system tools
   ;;
   2) echo "Creating tunned zip"
   cp scripts/updater-script-balanced META-INF/com/google/android/
   mv META-INF/com/google/android/updater-script-balanced META-INF/com/google/android/updater-script
-  zip XKernel-Flasher.zip -r META-INF presets system tools
   ;;
   3) echo "Creating zip with configured to extra performance"
   cp scripts/updater-script-performance META-INF/com/google/android/
   mv META-INF/com/google/android/updater-script-performance META-INF/com/google/android/updater-script
-  zip XKernel-Flasher.zip -r META-INF presets system tools
   ;;
   *) echo "Waiting for input"
   ;;
 esac
-mv XKernel-Flasher.zip signer/
+zip XKernel-Rolling.zip -r META-INF presets system tools
+mv XKernel-Rolling.zip signer/
 echo Signing zip file
-cd signer && java -jar signapk.jar testkey.x509.pem testkey.pk8 XKernel-Flasher.zip XKernel-Flasher-signed.zip
-rm XKernel-Flasher.zip
-mv XKernel-Flasher-signed.zip ../
+cd signer && java -jar signapk.jar testkey.x509.pem testkey.pk8 XKernel-Rolling.zip XKernel-Rolling-signed.zip
+echo Done!
+echo You may grab your zip file in XKernel-Flasher directory
+rm XKernel-Rolling.zip
+mv XKernel-Rolling-signed.zip ../
