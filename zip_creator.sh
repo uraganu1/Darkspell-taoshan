e570af55e3cb6888f82536f73b9f27e92bb2e8fd
sudokamikaze <pulshencode@outlook.com>
2016-03-30 22:03:13 +0300

    Added Copyright

--- zip_creator.sh
+++ zip_creator.sh
@@ -1,3 +1,18 @@
+#
+# Copyright � 2016, Pulshen Sudokamikaze "sudokamikaze" sudokamikaze.github.io
+#
+# Custom build script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#
 #!/bin/bash
 rm -rf XKernel-Flasher
 git clone https://github.com/Sudokamikaze/XKernel-Flasher-taoshan.git && cd XKernel-Flasher-taoshan
