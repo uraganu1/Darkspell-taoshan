2a1dc1cbe42ec9a6dbac71717dd8429acfe5a942
sudokamikaze <pulshencode@outlook.com>
2016-05-13 21:38:02 +0300

    scripts: Reworked build process, removed odd choose of presets

--- zip_creator.sh
+++ zip_creator.sh
@@ -20,30 +20,15 @@ cp ../arch/arm/boot/zImage tools/
 cp ../drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini system/etc/firmware/wlan/prima/
 eval $(grep CONFIG_LOCALVERSION= ../arch/arm/configs/pulshen_taoshan_defconfig)
 echo Welcome to XKernel flash zip creator
-echo =======================================
-echo 1 Default flash configuration
-echo 2 Tunned for battery and performance
-echo 3 Extra performance
-echo ========================================
-echo Current kernel version : $CONFIG_LOCALVERSION
-echo -n "Choose an action: "
-read item
-case "$item" in
-  1) echo "Creating default flash zip"
-  cp scripts/updater-script-default META-INF/com/google/android/
-  mv META-INF/com/google/android/updater-script-default META-INF/com/google/android/updater-script
-  ;;
-  2) echo "Creating tunned zip"
-  cp scripts/updater-script-balanced META-INF/com/google/android/
-  mv META-INF/com/google/android/updater-script-balanced META-INF/com/google/android/updater-script
-  ;;
-  3) echo "Creating zip with configured to extra performance"
-  cp scripts/updater-script-performance META-INF/com/google/android/
-  mv META-INF/com/google/android/updater-script-performance META-INF/com/google/android/updater-script
-  ;;
-  *) echo "Waiting for input"
-  ;;
-esac
+echo =================================================
+echo "Default preset have: "
+echo "zSwap = 10%"
+echo "Zen I/O sched"
+echo "Undervolted GPU"
+echo "Normal thermald configuration"
+echo ==================================================
+echo "U may tune kernel perfs by using Kernel Adiutor"
+echo ==================================================
 zip XKernel-Stable.zip -r META-INF presets system tools
 mv XKernel-Stable.zip signer/
 echo Signing zip file
