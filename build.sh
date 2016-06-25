b1d0e8f2854984841a1782f40415ab349daf49c5
sudokamikaze <pulshencode@outlook.com>
2016-06-25 19:17:19 +0300

    scripts: Updates, removed boot.img creation to due using zImage

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
  #
- # Copyright � 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+ # Copyright 2016, Pulshen Sudokamikaze "sudokamikaze"
  #
  # Custom build script
  #
@@ -14,8 +14,6 @@
  #
  #
 #!/bin/bash
-# export CROSS_COMPILE="/root/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-"
-# export CROSS_COMPILE="/root/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-"
 export CROSS_COMPILE="/home/kitt/Git/toolchains/4.9.3-2014.12.20141230.CR83/bin/arm-eabi-"
 STRIP="/home/kitt/Git/toolchains/4.9.3-2014.12.20141230.CR83/bin/arm-eabi-strip"
 MODULES_DIR="/home/kitt/Git/XKernel-taoshan/modules_dir"
@@ -45,7 +43,7 @@ case "$menu" in
   echo "1. Just build"
   echo "2. Build and shutdown"
   echo ==============================
-  echo -n "Choose an action: "
+  echo -n "Choose an option: "
   read option
   if [ $option == 1 ]; then make pulshen_taoshan_defconfig; make -j5
 elif [ $option == 2 ]; then pulshen_taoshan_defconfig; make -j5; poweroff
@@ -64,8 +62,6 @@ elif [ $option == 2 ]; then pulshen_taoshan_defconfig; make -j5; poweroff
   *) echo Unknown symbol
   ;;
 esac
-# make pulshen_taoshan_defconfig
-# make -j5
 if [ -a $ZIMAGE ];
 then
 echo "Copying modules"
@@ -76,10 +72,10 @@ echo "Stripping modules for size"
 $STRIP --strip-unneeded *.ko
 zip -9 modules *
 cd $KERNEL_DIR
-echo "Creating boot image"
-$MKBOOTFS ramdisk/ > $KERNEL_DIR/ramdisk.cpio
-cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
-$MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o $KERNEL_DIR/boot.img
+# echo "Creating boot image"
+# $MKBOOTFS ramdisk/ > $KERNEL_DIR/ramdisk.cpio
+# cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
+# $MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o $KERNEL_DIR/boot.img
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
