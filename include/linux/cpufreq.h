2a3ebe69cd76e39ff6b9c612d885fef37e6df39e
sudokamikaze <pulshencode@outlook.com>
2016-06-29 16:46:42 +0300

    governors: Added bioshock

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -380,6 +380,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_bioshock)
 #endif
 
 
