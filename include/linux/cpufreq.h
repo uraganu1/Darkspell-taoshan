ef521a9d20c04afcbb03e1e43f3f18c155b98aaa
sudokamikaze <pulshencode@outlook.com>
2016-07-13 16:36:43 +0300

    governors: Added intelliactive

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -383,6 +383,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intelliactive)
 #endif
 
 
