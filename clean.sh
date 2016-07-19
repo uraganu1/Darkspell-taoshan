e927447066d28d66db1e920f2c2384fe716482e9
sudokamikaze <pulshencode@outlook.com>
2016-07-19 23:11:42 +0300

    scripts: Updated cleaner script

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
-KERNEL_DIR="/home/kitt/Git/XKernel-taoshan_mm/"
+DIR=$(pwd)
+KERNEL_DIR="$DIR"
 make ARCH=arm CROSS_COMPILE="/home/kitt/Git/toolchains/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-" -j5 clean mrproper
 rm -rf $KERNEL_DIR/ramdisk.cpio
 rm -rf $KERNEL_DIR/root.fs
