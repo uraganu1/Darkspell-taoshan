430eccaa0ccc94887165f886a2db249d94e53443
Sudokamikaze <pulshencode@outlook.com>
2016-08-23 03:59:47 +0300

    scripts: Added $USER var

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 DIR=$(pwd)
 KERNEL_DIR="$DIR"
-make ARCH=arm CROSS_COMPILE="/home/kitt/Git/toolchains/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-" -j5 clean mrproper
+make ARCH=arm CROSS_COMPILE="/home/$USER/Git/toolchains/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-" -j5 clean mrproper
 rm -rf $KERNEL_DIR/ramdisk.cpio
 rm -rf $KERNEL_DIR/root.fs
 rm -rf $KERNEL_DIR/boot.img
