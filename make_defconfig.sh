cd44bf84d6b8567523270176d78381ad5ee2a1c8
Sudokamikaze <pulshencode@outlook.com>
2016-11-09 23:41:05 +0200

    scripts: Implement commit funct

--- make_defconfig.sh
+++ make_defconfig.sh
@@ -5,3 +5,19 @@ make pulshen_taoshan_defconfig
 make menuconfig
 make savedefconfig && cp defconfig arch/arm/configs/pulshen_taoshan_defconfig
 rm defconfig
+ITSME=$(git config user.name)
+echo "Do you wan't to commit? "
+read douwant
+case "$douwant" in
+  y|Y)
+if [ $ITSME == Sudokamikaze ]; then
+echo "defconfig: "
+read commitmsg
+git add arch/arm/configs/pulshen_taoshan_defconfig
+git commit -S -m "defconfig: $commitmsg"
+git push
+fi
+;;
+  n|N) exit
+;;
+esac
