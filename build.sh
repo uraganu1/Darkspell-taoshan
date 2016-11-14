3413ed57ff4e5ed11f110b04657f05513bbc6d95
Sudokamikaze <pulshencode@outlook.com>
2016-11-14 17:27:26 +0200

    scripts: Add option to build only zimage

--- build.sh
+++ build.sh
@@ -14,6 +14,9 @@
 #
 #
 
+# Define true if you don't want to build boot.img
+ONLYZIMAGE=true
+
 # Define here your toolchain path
 export CROSS_COMPILE="$HOME/Git/toolchains/architoolchain-4.8-a15/bin/arm-eabi-"
 DIR=$(pwd)
@@ -38,10 +41,12 @@ make pulshen_taoshan_defconfig
 make -j5
 if [ -a $ZIMAGE ];
 then
+if [ "$ONLYZIMAGE" == "false" ]; then
 echo "Creating boot image"
 $MKBOOTFS ramdisk/ > $KERNEL_DIR/ramdisk.cpio
 cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
 $MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o $KERNEL_DIR/boot.img
+fi
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
