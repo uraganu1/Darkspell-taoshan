bd11fee181a1915b5ce6926c466f92d937bcf771
Sudokamikaze <pulshencode@outlook.com>
2017-01-19 21:28:50 +0200

    scripts: Merge new system of scripts

--- clean.sh
+++
@@ -1,9 +0,0 @@
-DIR=$(pwd)
-KERNEL_DIR="$DIR"
-make ARCH=arm CROSS_COMPILE="$HOME/Git/toolchains/4.8.3-2013.11.20131205/bin/arm-linux-gnueabihf-" -j5 clean mrproper
-rm -rf $KERNEL_DIR/ramdisk.cpio
-rm -rf $KERNEL_DIR/root.fs
-rm -rf $KERNEL_DIR/boot.img
-rm -rf Darkspell-Flasher-taoshan
-rm -rf ramdisk_editor
-rm -rf modules_dir/*
